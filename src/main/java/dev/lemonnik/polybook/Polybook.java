fb0ddca1b3d53a4c7107742e37137f204ca05bf8
Lemonnik6484 <LemonGamer6484@gmail.com>
2025-02-02 22:42:43 +0100

    Initial commit

---
+++ src/main/java/dev/lemonnik/polybook/Polybook.java
@@ -0,0 +1,33 @@
+package dev.lemonnik.polybook;
+
+import dev.lemonnik.polybook.items.PolybookItem;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Polybook implements ModInitializer {
+	public static final String MOD_ID = "polybook";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		final Item POLYBOOK = Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "polydex"), new PolybookItem(
+				new Item.Settings()
+						.maxCount(1)
+						.registryKey(
+								RegistryKey.of(
+										RegistryKeys.ITEM,
+										Identifier.of(MOD_ID, "polydex")
+								)
+						)
+		));
+	}
+}
---
+++ src/main/java/dev/lemonnik/polybook/items/PolybookItem.java
@@ -0,0 +1,41 @@
+package dev.lemonnik.polybook.items;
+
+import dev.lemonnik.polybook.Polybook;
+import eu.pb4.polymer.core.api.item.PolymerItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import xyz.nucleoid.packettweaker.PacketContext;
+
+public class PolybookItem extends Item implements PolymerItem {
+    public PolybookItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        if (!world.isClient) {
+            if (user.isPlayer()) {
+                user.getServer().getCommandManager().executeWithPrefix(user.getCommandSource((ServerWorld) world), "/polydex");
+            }
+        }
+        return super.use(world, user, hand);
+    }
+
+    @Override
+    public @Nullable Identifier getPolymerItemModel(ItemStack stack, PacketContext context) {
+        return null;
+    }
+
+    @Override
+    public Item getPolymerItem(ItemStack itemStack, PacketContext packetContext) {
+        return Items.KNOWLEDGE_BOOK;
+    }
+}

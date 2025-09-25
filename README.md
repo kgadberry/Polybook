d3a95ed6a22b85766336af7cbb5257537b390d83
K. Gadberry <1781520+kgadberry@users.noreply.github.com>
2025-09-25 21:19:58 +0000

    Changed recipe and fixed item name/tooltip

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Polybook
-Polybook is [Polymer](https://modrinth.com/mod/polymer) mod that adds server-side item called **Polydex**. It was made to make usage of [Polydex](https://modrinth.com/mod/polydex) feel more vanilla
+Polybook is [Polymer](https://modrinth.com/mod/polymer) mod that adds a craftable Knowledge Book item to the game that opens the Polydex menu. It was made to make the usage of [Polydex](https://modrinth.com/mod/polydex) more intuitive.
 
-Right-click to open Polydex menu, instead of using command
+Right-click with the item to open Polydex menu, instead of using `/polydex`.
 
-Recipe is simple: one block of any leaves and one book
-![Polydex recipe](https://cdn.modrinth.com/data/cached_images/406711a9661276ca6d9bd09edf67957677743bbd.png)
+![Polybook Recipe](https://gist.github.com/kgadberry/283277c008ca0da6c9e40931a88040e6/raw/0cc4b6761e37993aa20d91d158fa8a8a1ad91e0c/crafting-grid.png)
--- src/main/java/dev/lemonnik/polybook/Polybook.java
+++ src/main/java/dev/lemonnik/polybook/Polybook.java
@@ -4,12 +4,17 @@ import dev.lemonnik.polybook.items.PolybookItem;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
+import net.minecraft.component.type.LoreComponent;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,16 +24,22 @@ public class Polybook implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		final Item POLYDEX = Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "polydex"), new PolybookItem(
-				new Item.Settings()
-						.maxCount(1)
-						.registryKey(
-								RegistryKey.of(
-										RegistryKeys.ITEM,
-										Identifier.of(MOD_ID, "polydex")
-								)
-						)
-		));
+	final Item POLYDEX = Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "polydex"), new PolybookItem(
+		new Item.Settings()
+			.maxCount(1)
+			.component(
+				DataComponentTypes.LORE,
+				new LoreComponent(List.of(
+					Text.translatable("item.polybook.polydex.tooltip").formatted(Formatting.GRAY)
+				))
+			)
+			.registryKey(
+				RegistryKey.of(
+					RegistryKeys.ITEM,
+					Identifier.of(MOD_ID, "polydex")
+				)
+			)
+	));
 
 		PolymerResourcePackUtils.addModAssets(MOD_ID);
 	}
